7bd9c621f1b4d14f72b489a13d83e9ad3e631ad4
ArelGindos <arel-g@hotmail.com>
2016-07-08 19:06:01 +0300

    fixed ComboBox

---
+++ Form/CompleteForm/ComboBox.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Radiolist.h"
+#include "Button.h"
+#include <Windows.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+/*A class that implements a Combo Box Widget*/
+class ComboBox2 : public Panel2, public MouseListener2
+{
+private:
+	size_t index;
+	bool drop_down;
+	void clearLabel(Graphics &g, int left, int top, int layer) const;
+	void clearBody(Graphics &g, int left, int top, int layer) const;
+public:
+
+	//A constructor that gets the starting coordinate of the Combo Box and the list of items
+	ComboBox2(int _width, vector<string> options);
+
+	//Method that responds to key events
+	void keyDown(KEY_EVENT_RECORD key) { Panel2::keyDown(key); }
+
+	//Method that responds to mouse events
+	void mousePressed(int x, int y) { Panel2::mousePressed(x, y); }
+
+	virtual void mousePressed(Control* widget, int x, int y, bool isLeft);
+
+	void printWidget() {};
+
+	void addControl(Control& control, int x, int y) = delete;
+
+	virtual void draw(Graphics &g, int left, int top, int layer);
+
+	void setSelectedIndex(size_t i) { index = i; };
+
+	size_t getSelectedIndex() const { return index; }
+};
+#pragma once
