b5f3ba1ddf8a337e275bcc45f07bc1d2cb61dbf4
ArelGindos <arel-g@hotmail.com>
2016-07-14 20:30:06 +0300

    changed Enums.h

---
+++ Form/CompleteForm/Enums.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+Enum that for special keys
+*/
+enum  Keys { UP, DOWN, LEFT, RIGHT, BACKSPACE, ENTER, TAB, SPACEBAR, DEL, INSERT, OTHER, KEY_RELEASED };
+
+enum class Color { Red, Blue, Green, Purple, Teal, Yellow, White, Black , Cyan, Orange};
+
+enum class BorderType { Single, Double, None };
