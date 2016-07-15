0931ac83b041ff9c6aa7f605d2c9beb949fcdad8
shnors <talshnors@gmail.com>
2016-07-15 15:18:00 +0300

    removed enrelated files from git

--- Form/CompleteForm/Enums.h
+++ Form/CompleteForm/Enums.h
@@ -1,6 +1,10 @@
 #pragma once
-enum  class Keys { UP, DOWN, LEFT, RIGHT, BACKSPACE, ENTER, TAB, SPACEBAR, DEL, INSERT, OTHER, KEY_RELEASED };
 
-enum class Color { Black, Blue, Green, Red, Cyan, Purple, Orange, White };
+/*
+Enum that for special keys
+*/
+enum  Keys { UP, DOWN, LEFT, RIGHT, BACKSPACE, ENTER, TAB, SPACEBAR, DEL, INSERT, OTHER, KEY_RELEASED, ESCAPE };
+
+enum class Color { Red, Blue, Green, Purple, Teal, Yellow, White, Black , Cyan, Orange};
 
 enum class BorderType { Single, Double, None };
--- Form/CompleteForm/MouseListener2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Control.h"
-
-struct MouseListener2
-{
-	virtual void mousePressed(Control* control, int x, int y, bool isLeft) = 0;
-};
